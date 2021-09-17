9ed40e3c4fe824ba2434c745f9fb67283350772e
p82222 <42255490+p82222@users.noreply.github.com>
2021-09-17 10:59:52 -0400

    add main methon in the addressbook class

---
+++ src/AddressBook.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+
+    private List<BuddyInfo> myList;
+
+    public AddressBook() {
+        myList = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        myList.add(buddy);
+
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        for (int counter = 0; counter < myList.size(); counter++) {
+            if (myList.get(counter).equals(buddy)) {
+                myList.remove(counter);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Address Book");
+    }
+}
